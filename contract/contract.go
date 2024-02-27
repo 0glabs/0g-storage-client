58c1781e7f05bb68c87749d0a8949f8b67882477
Bo QIU <35757521+boqiu@users.noreply.github.com>
2024-02-27 11:41:21 +0800

    Add method for submission to calculate root hash (#2)

--- contract/contract.go
+++ contract/contract.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/openweb3/web3go"
 	"github.com/zero-gravity-labs/zerog-storage-client/common/blockchain"
 )
@@ -36,4 +37,22 @@ func (submission Submission) String() string {
 	}
 
 	return fmt.Sprintf("{ Size: %v, Heights: %v }", submission.Length, heights)
+}
+
+func (submission Submission) Root() common.Hash {
+	numNodes := len(submission.Nodes)
+
+	// should be never occur
+	if numNodes == 0 {
+		return common.Hash{}
+	}
+
+	// calculate root in reverse order
+	root := submission.Nodes[numNodes-1].Root
+	for i := 1; i < numNodes; i++ {
+		left := submission.Nodes[numNodes-1-i]
+		root = crypto.Keccak256Hash(left.Root[:], root[:])
+	}
+
+	return root
 }
