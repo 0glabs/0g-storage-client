bac7e4c547446d26f02b86d1a6064e06aae8f76d
Conflux <wanliqunn@163.com>
2024-09-10 18:24:23 +0800

    feat: Add support for downloading folders to 0gStorage

--- cmd/download.go
+++ cmd/download.go
@@ -8,22 +8,40 @@ import (
 	"github.com/0glabs/0g-storage-client/indexer"
 	"github.com/0glabs/0g-storage-client/node"
 	"github.com/0glabs/0g-storage-client/transfer"
+	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
+type downloadArgument struct {
+	file string
+
+	indexer string
+	nodes   []string
+
+	root  string
+	proof bool
+
+	timeout time.Duration
+}
+
+func bindDownloadFlags(cmd *cobra.Command, args *downloadArgument) {
+	cmd.Flags().StringVar(&args.file, "file", "", "File name to download")
+	cmd.MarkFlagRequired("file")
+
+	cmd.Flags().StringSliceVar(&args.nodes, "node", []string{}, "ZeroGStorage storage node URL. Multiple nodes could be specified and separated by comma, e.g. url1,url2,url3")
+	cmd.Flags().StringVar(&args.indexer, "indexer", "", "ZeroGStorage indexer URL")
+	cmd.MarkFlagsOneRequired("indexer", "node")
+
+	cmd.Flags().StringVar(&args.root, "root", "", "Merkle root to download file")
+	cmd.MarkFlagRequired("root")
+	cmd.Flags().BoolVar(&args.proof, "proof", false, "Whether to download with merkle proof for validation")
+
+	cmd.Flags().DurationVar(&args.timeout, "timeout", 0, "cli task timeout, 0 for no timeout")
+}
+
 var (
-	downloadArgs struct {
-		file string
-
-		indexer string
-		nodes   []string
-
-		root  string
-		proof bool
-
-		timeout time.Duration
-	}
+	downloadArgs downloadArgument
 
 	downloadCmd = &cobra.Command{
 		Use:   "download",
@@ -33,18 +51,7 @@ var (
 )
 
 func init() {
-	downloadCmd.Flags().StringVar(&downloadArgs.file, "file", "", "File name to download")
-	downloadCmd.MarkFlagRequired("file")
-
-	downloadCmd.Flags().StringSliceVar(&downloadArgs.nodes, "node", []string{}, "ZeroGStorage storage node URL. Multiple nodes could be specified and separated by comma, e.g. url1,url2,url3")
-	downloadCmd.Flags().StringVar(&downloadArgs.indexer, "indexer", "", "ZeroGStorage indexer URL")
-	downloadCmd.MarkFlagsOneRequired("indexer", "node")
-
-	downloadCmd.Flags().StringVar(&downloadArgs.root, "root", "", "Merkle root to download file")
-	downloadCmd.MarkFlagRequired("root")
-	downloadCmd.Flags().BoolVar(&downloadArgs.proof, "proof", false, "Whether to download with merkle proof for validation")
-
-	downloadCmd.Flags().DurationVar(&downloadArgs.timeout, "timeout", 0, "cli task timeout, 0 for no timeout")
+	bindDownloadFlags(downloadCmd, &downloadArgs)
 
 	rootCmd.AddCommand(downloadCmd)
 }
@@ -57,28 +64,42 @@ func download(*cobra.Command, []string) {
 		defer cancel()
 	}
 
-	if downloadArgs.indexer != "" {
-		indexerClient, err := indexer.NewClient(downloadArgs.indexer, indexer.IndexerClientOption{
-			ProviderOption: providerOption,
-			LogOption:      common.LogOption{Logger: logrus.StandardLogger()},
-		})
-		if err != nil {
-			logrus.WithError(err).Fatal("Failed to initialize indexer client")
-		}
-		if err := indexerClient.Download(ctx, downloadArgs.root, downloadArgs.file, downloadArgs.proof); err != nil {
-			logrus.WithError(err).Fatal("Failed to download file from indexer")
-		}
-		return
-	}
-
-	nodes := node.MustNewZgsClients(downloadArgs.nodes, providerOption)
-
-	downloader, err := transfer.NewDownloader(nodes, common.LogOption{Logger: logrus.StandardLogger()})
+	downloader, closer, err := newDownloader(downloadArgs)
 	if err != nil {
 		logrus.WithError(err).Fatal("Failed to initialize downloader")
 	}
+	defer closer()
 
 	if err := downloader.Download(ctx, downloadArgs.root, downloadArgs.file, downloadArgs.proof); err != nil {
 		logrus.WithError(err).Fatal("Failed to download file")
 	}
+}
+
+func newDownloader(args downloadArgument) (transfer.IDownloader, func(), error) {
+	if args.indexer != "" {
+		indexerClient, err := indexer.NewClient(args.indexer, indexer.IndexerClientOption{
+			ProviderOption: providerOption,
+			LogOption:      common.LogOption{Logger: logrus.StandardLogger()},
+		})
+		if err != nil {
+			return nil, nil, errors.WithMessage(err, "failed to initialize indexer client")
+		}
+
+		return indexerClient, indexerClient.Close, nil
+	}
+
+	clients := node.MustNewZgsClients(args.nodes, providerOption)
+	closer := func() {
+		for _, client := range clients {
+			client.Close()
+		}
+	}
+
+	downloader, err := transfer.NewDownloader(clients, common.LogOption{Logger: logrus.StandardLogger()})
+	if err != nil {
+		closer()
+		return nil, nil, err
+	}
+
+	return downloader, closer, nil
 }
---
+++ transfer/download/downloading_dir.go
@@ -0,0 +1,100 @@
+package download
+
+import (
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/0glabs/0g-storage-client/transfer/dir"
+	"github.com/pkg/errors"
+)
+
+type DownloadingDir struct {
+	filename string // The original directory name before downloading
+}
+
+// CreateDownloadingDir creates a temporary downloading directory by renaming the existing directory if it exists
+// or by creating a new one if it doesn't. It ensures files are stored in a safe temporary directory.
+func CreateDownloadingDir(filename string) (*DownloadingDir, error) {
+	tmpDir := filename + downloadingFileSuffix
+
+	// Attempt to rename the existing directory to the temporary downloading directory.
+	err := os.Rename(filename, tmpDir)
+	if err == nil {
+		return &DownloadingDir{filename}, nil
+	}
+
+	// If the directory doesn't exist, create the temporary directory.
+	if os.IsNotExist(err) {
+		if err := os.MkdirAll(tmpDir, 0755); err != nil {
+			return nil, errors.WithMessage(err, "failed to create temporary directory")
+		}
+		return &DownloadingDir{filename}, nil
+	}
+
+	return nil, errors.WithMessage(err, "failed to rename existing directory")
+}
+
+// Add adds a file, directory, or symbolic link to the downloading directory.
+func (directory *DownloadingDir) Add(node *dir.FsNode, relpath string, persist func(path string) error) error {
+	savePath := filepath.Join(directory.filename+downloadingFileSuffix, relpath)
+
+	// Use the custom persist function if provided
+	if persist != nil {
+		return persist(savePath)
+	}
+
+	// Handle different file types if no custom persist function is provided.
+	switch node.Type {
+	case dir.FileTypeFile:
+		// Create an empty file (touch) at the save path.
+		if err := touchFile(savePath); err != nil {
+			return errors.WithMessagef(err, "failed to create empty file %s", savePath)
+		}
+	case dir.FileTypeDirectory:
+		// Create the directory if it doesn't exist.
+		if err := os.MkdirAll(savePath, 0755); err != nil {
+			return errors.WithMessagef(err, "failed to create directory %s", savePath)
+		}
+	case dir.FileTypeSymbolic:
+		// Create a symbolic link at the specified path.
+		if err := os.Symlink(node.Link, savePath); err != nil {
+			return errors.WithMessagef(err, "failed to create symbolic link %s", savePath)
+		}
+	default:
+		return errors.Errorf("unknown file type: %v", node.Type)
+	}
+
+	return nil
+}
+
+// Seal finalizes the downloading process by renaming the temporary directory back to its original name.
+// It should be called after all files have been added to the directory.
+func (directory *DownloadingDir) Seal() error {
+	tmpDir := directory.filename + downloadingFileSuffix
+
+	// Rename the temporary directory back to the original directory name.
+	if err := os.Rename(tmpDir, directory.filename); err != nil {
+		return errors.WithMessage(err, "failed to rename directory")
+	}
+
+	return nil
+}
+
+// touchFile creates or updates the access and modification time of a file.
+func touchFile(filePath string) error {
+	// Open the file, create it if it doesn't exist
+	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return errors.WithMessage(err, "failed to create or open file")
+	}
+	defer file.Close()
+
+	// Update the file's access and modification times to the current time
+	currentTime := time.Now()
+	if err := os.Chtimes(filePath, currentTime, currentTime); err != nil {
+		return errors.WithMessage(err, "failed to change file timestamps")
+	}
+
+	return nil
+}
